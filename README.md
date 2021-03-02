ba80b5b484389e6677b4e5cdd8dc2c7cded7aa1b
David Leon <jelid.leon@gmail.com>
2021-03-02 17:18:15 -0600

    Add the first part of the gqlgen start

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # learn-gqlgen
 My history about the process to learn GraphQL+Go
+
+## Step 1. Setup gqlgen
+
+Do next, with the current code, and it will run like the example from https://gqlgen.com/getting-started/.
+
+```bash
+$ mkdir learn-gqlgen
+$ cd learn-gqlgen/
+$ go mod init github.com/eisenheimjelid/learn-gqlgen
+$ go get github.com/99designs/gqlgen
+$ go run github.com/99designs/gqlgen init
+Exec "go run ./server.go" to start GraphQL server
+$ go run server.go 
+2021/03/02 16:29:48 connect to http://localhost:8080/ for GraphQL playground
+```
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,36 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+
+	"github.com/eisenheimjelid/learn-gqlgen/graph/generated"
+	"github.com/eisenheimjelid/learn-gqlgen/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Intn(100)),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
